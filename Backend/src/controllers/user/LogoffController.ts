c5192ada9661c07efca2125a57b254cbfd663c1f
danielosilva1 <danielosilva@alu.ufc.br>
2023-11-02 21:49:29 -0300

    [#2] Correção do valor passado para a função clearCookie: token não estava sendo removido após requisição de logoff

--- Backend/src/controllers/user/LogoffController.ts
+++ Backend/src/controllers/user/LogoffController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 
 class LogoffController {
     handle(req: Request, res: Response) {
-        return res.clearCookie("acces_token").status(200).json({mensagem: "Logoff realizado com sucesso"});
+        return res.clearCookie("access_token").status(200).json({mensagem: "Logoff realizado com sucesso"});
     }
 }
 
