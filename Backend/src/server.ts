0ee09fdf64a0f6b0f2531bac22fd3c47bacbf48a
danielosilva1 <danielosilva@alu.ufc.br>
2023-11-25 20:48:19 -0300

    Inclusão do pacote cors para permitir rodar servidores do front e back

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import { router } from './routes'
 
 const cookieParser = require('cookie-parser');
+const cors = require('cors')
 
 const app = express();
 app.use('/uploads', express.static('uploads'));
 
+app.use(cors())
 app.use(express.json());
 app.use(cookieParser());
 app.use(router);
