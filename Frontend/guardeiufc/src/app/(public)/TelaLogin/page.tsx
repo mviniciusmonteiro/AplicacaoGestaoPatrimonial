8056bc6d6d7aa25f06c7e8863bec95dce558e59a
danielosilva1 <danielosilva@alu.ufc.br>
2023-11-26 00:29:37 -0300

    [#17] Integração dos componentes já implementados da Tela de Login

--- Frontend/guardeiufc/src/app/(public)/TelaLogin/page.tsx
+++ Frontend/guardeiufc/src/app/(public)/TelaLogin/page.tsx
@@ -5,6 +5,8 @@ import image from "/public/Computer login-rafiki (1).png";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 interface FormData {
   nome: string;
@@ -27,14 +29,48 @@ function TelaLogin() {
     }));
   };
 
-  const handleLogin = (): void => {
-    // Simule uma verificação de login aqui
-    if (formData.nome === "usuario" && formData.senha === "senha") {
-      alert("Login bem-sucedido!");
-      router.push("/TelaAdministrador");
-    } else {
-      alert("Nome de usuário ou senha inválidos. Tente novamente.");
+  const validateData = (username: String, password: String) => {
+    if (username == '' || password == '') {
+      return false;
     }
+    return true;
+  }
+
+  const handleLogin = (): void => {
+    const dataIsValid = validateData(formData.nome, formData.senha);
+    if (!dataIsValid) {
+      Swal.fire({
+        icon: 'warning',
+        text: 'Informe nome de usuário e senha para realizar login!'
+      });
+      return;
+    }
+    axios.post(process.env.NEXT_PUBLIC_BASE_URL + '/login', {
+      username: formData.nome,
+      password: formData.senha
+    }).then((response) => {
+      if (response.status == 200) {
+        if (response.data.isAdmin) {
+          router.push('/TelaAdministrador');
+        } else {
+          // router.push('/TelaComum');
+        }
+      }
+    }).catch((error) => {
+      if (error.response.status == 400) {
+        Swal.fire({
+          icon: 'error',
+          text: 'Nome de usuário e/ou senha incorretos. Tente novamente!',
+          footer: '<a href="/TelaSolicitarCodigo">Esqueceu sua senha?</a>'
+        });
+      } else {
+        Swal.fire({
+          icon: 'error',
+          text: `Ocorreu um erro ao tentar fazer login.\nStatus do erro: (${error.response.status})`
+        });
+      }
+      console.error(error);
+    });
   };
 
   return (
@@ -61,24 +97,27 @@ function TelaLogin() {
               placeholder="Digite seu nome de usuário"
               onChange={handleInputChange}
               className={styles.input}
+              tabIndex={0}
             />
             <p className={styles.Nomes}>Senha</p>
             <input
-              type="text"
+              type="password"
               id="senha"
               name="senha"
               placeholder="Digite sua senha"
               value={formData.senha}
               className={styles.input}
               onChange={handleInputChange}
+              tabIndex={0}
             />
           </div>
-          <div className={styles.botaoLogin}>
+          <div className={styles.botaoLogin} tabIndex={0}>
             <p className={styles.textoBotao} onClick={handleLogin}>Login</p>
           </div>
           <div className={styles.textosSaida}>
-            <p>Esqueceu a senha?</p>
-            <Link href = "/TelaCadastro">Não possui uma conta? Cadastre -se </Link>
+            <Link href = "/TelaSolicitarCodigo" tabIndex={0}>Esqueceu a senha?</Link>
+            <br></br>
+            <Link href = "/TelaCadastro" tabIndex={0}>Não possui uma conta? Cadastre-se</Link>
           </div>
         </div>
       </div>
